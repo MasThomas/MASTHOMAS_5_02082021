cdc72b11b2103b81a4db8522bb5914d3c15ad780
MasThomas <82974068+MasThomas@users.noreply.github.com>
2021-08-28 19:52:48 +0100

    Exporting constructor to a new .js file

---
+++ camera.js
@@ -0,0 +1,13 @@
+// Construtor 
+
+class Product {
+    constructor({lenses,_id,name,price,description,imageUrl,quantity,}) {
+        this.lenses = lenses;
+        this.id = _id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.quantity = quantity;
+    }
+}
