a5b605611dc1d7b9d5bd8fe3789248e60e556311
MasThomas <82974068+MasThomas@users.noreply.github.com>
2021-09-01 14:23:00 +0100

    new func to calculate totalPrice, display modifs

--- cart.js
+++ cart.js
@@ -1,13 +1,14 @@
 // Vérification si le panier est vide
 
 var containerCart = document.getElementById("tbody")
+var containerMessage = document.getElementById("container-alert-message")
 
 var cart = JSON.parse(localStorage.getItem("cartStorage"))
 
 function checkEmptyCart() {
     if (localStorage.getItem("cartStorage") === null) {
-        containerCart.innerHTML =
-            `<div class="my-3">
+        containerMessage.innerHTML =
+            `<div class="my-5">
             <p class="text-center my-3">Votre panier est vide !<br><br>
             Afin de retourner à la liste des articles, veuillez cliquer sur le bouton ci-dessous
             </p>
@@ -26,13 +27,14 @@ function checkEmptyCart() {
                         <div class="col-sm-2 hidden-xs"><img src="${item.image}" alt="Thumbnail de l'appareil ${item.name}" class="img-thumbnail "/></div>
                         <div class="col-sm-10">
                             <h4 class="nomargin">${item.name}</h4>
+                            <p>${item.description}</p>
                         </div>
                         </div>
                     </td>
                     <td data-th="Lentille choisie">${item.lenses}</td>
                     <td data-th="Price" class="text-center">${item.price/100} €</td>
                     <td data-th="Quantity" class="text-center">${item.quantity}</td>
-                    <td data-th="Subtotal" class="text-center"></td>
+                    <td data-th="Subtotal" class="text-center subtotal">${item.subtotal/100} €</td>
                     </tr>
                 </tbody>`
         }
@@ -46,7 +48,7 @@ var formDiv = document.querySelector("#form-container")
 var tableFooter = document.querySelector("#table_footer")
 var buttonDeleteCart = document.querySelector("#deleteCart")
 
-function doesFormTableAndDeleteButtonAppear() {
+function doesFormAndTableAndDeleteButtonAppear() {
     if (localStorage.getItem("cartStorage") === null) {
         formDiv.remove()
         tableFooter.remove()
@@ -60,7 +62,23 @@ buttonDeleteCart.addEventListener('click', function(){
     console.log(localStorage)
 })
 
+var totalPrice = 0;
+
+function calculateTotalPrice(){
+    for(let i = 0; i < cart.length; ++i) {
+        let j = Object.values(cart[i])
+        totalPrice += (j[6]/100);
+    }
+}
+
+var totalPriceContainer = document.getElementById("totalPrice")
+function displayTotalPrice() {
+    totalPriceContainer.innerHTML += `${totalPrice} €`;
+}
+
 fetch("http://localhost:3000/api/cameras/")
     .then(res => res.json)
-    .then(doesFormTableAndDeleteButtonAppear())
+    .then(doesFormAndTableAndDeleteButtonAppear())
     .then(checkEmptyCart())
+    .then(calculateTotalPrice())
+    .then(displayTotalPrice())
