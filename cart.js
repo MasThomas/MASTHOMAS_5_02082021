1bdfe3ba98a924c476f04910c0862111a862d67e
MasThomas <82974068+MasThomas@users.noreply.github.com>
2021-09-02 13:55:42 +0100

    var2let&creating form validation function&api post

--- cart.js
+++ cart.js
@@ -1,9 +1,9 @@
 // Vérification si le panier est vide
 
-var containerCart = document.getElementById("tbody")
-var containerMessage = document.getElementById("container-alert-message")
+let containerCart = document.getElementById("tbody")
+let containerMessage = document.getElementById("container-alert-message")
 
-var cart = JSON.parse(localStorage.getItem("cartStorage"))
+let cart = JSON.parse(localStorage.getItem("cartStorage"))
 
 function checkEmptyCart() {
     if (localStorage.getItem("cartStorage") === null) {
@@ -44,9 +44,9 @@ function checkEmptyCart() {
 
 // Si le panier est vide, supression du formulaire, du bouton pour vider le panier ainsi que d'un bout du tableau
 
-var formDiv = document.querySelector("#form-container")
-var tableFooter = document.querySelector("#table_footer")
-var buttonDeleteCart = document.querySelector("#deleteCart")
+let formDiv = document.querySelector("#form-container")
+let tableFooter = document.querySelector("#table_footer")
+let buttonDeleteCart = document.querySelector("#deleteCart")
 
 function doesFormAndTableAndDeleteButtonAppear() {
     if (localStorage.getItem("cartStorage") === null) {
@@ -62,19 +62,156 @@ buttonDeleteCart.addEventListener('click', function(){
     console.log(localStorage)
 })
 
-var totalPrice = 0;
+let totalPrice = 0;
 
 function calculateTotalPrice(){
     for(let i = 0; i < cart.length; ++i) {
         let j = Object.values(cart[i])
-        totalPrice += (j[6]/100);
+        totalPrice += (j[7]/100);
     }
 }
 
-var totalPriceContainer = document.getElementById("totalPrice")
+let totalPriceContainer = document.getElementById("totalPrice")
 function displayTotalPrice() {
     totalPriceContainer.innerHTML += `${totalPrice} €`;
 }
+
+
+// Validation du formulaire
+
+let submitBtn = document.getElementById("submitForm")
+let firstNameInput = document.getElementById("inputFirstName")
+let lastNameInput = document.getElementById("inputLastName")
+let emailInput = document.getElementById("inputEmail")
+let adressInput = document.getElementById("inputAddress")
+let cityInput = document.getElementById("inputCity")
+
+
+let products = []
+
+function getCartIds() {
+    
+    for(let i = 0; i < cart.length; ++i) {
+        let j = Object.values(cart[i])        
+        products.push(j[0]);
+    }
+    return products;
+}
+
+
+submitBtn.addEventListener('click', submitVerificatons)
+
+function submitVerificatons() {
+    inputFirstNameVerif()
+    console.log(inputFirstNameVerif())
+    inputLastNameVerif()
+    console.log(inputLastNameVerif())
+    inputEmailVerif()
+    console.log(inputEmailVerif())
+    inputAdressVerif()
+    console.log(inputAdressVerif())
+    inputCityVerif()
+    console.log(inputCityVerif())
+
+    // Création de l'objet à envoyer
+
+    let contact = {
+        firstName: firstNameInput.value,
+        lastName: lastNameInput.value,
+        address: adressInput.value,
+        city: cityInput.value,
+        email: emailInput.value
+    }
+
+    if (inputFirstNameVerif() === true && inputLastNameVerif() === true && inputEmailVerif() === true && inputAdressVerif() === true && inputCityVerif() === true) {
+            
+            getCartIds()
+            let stringifySentInformations = JSON.stringify({contact, products});
+            console.log(stringifySentInformations)
+
+            // Envoyer l'objet en POST
+            fetch("http://localhost:3000/api/cameras/order", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: stringifySentInformations
+            })
+
+            // Traiter la réponse, et stocker l'orderId retourné dans le localStorage
+
+            .then(response => response.json()) 
+            .then(response => { localStorage.setItem("orderId",JSON.stringify(response.orderId))})
+            localStorage.setItem("totalPrice", totalPrice)
+            console.log(localStorage)
+                            
+            // Passer à la page suivante
+
+            // window.location.href= "./confirmation.html"
+
+
+    } else {
+        alert('Veuillez vérifier la validité des informations saisies')
+        console.log("Mauvaise saisie")
+    }
+}
+ 
+
+// Regex Nom, Prénom et Ville permettant les accents, les apostrophes, les tirets.
+
+function inputFirstNameVerif(inputFirstName) {
+    let regex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+    let ctrl = document.getElementById("inputFirstName")
+    if (regex.test(ctrl.value) === true) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inputLastNameVerif(lastNameInput) {
+    let regex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+    let ctrl = document.getElementById("inputLastName")
+    if (regex.test(ctrl.value) === true) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// Regex email conforme à la norme RFC 5322
+
+function inputEmailVerif(emailInput) {
+    let regex = /(?:[a-z0-9!#$%&'*+\=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    let ctrl = document.getElementById("inputEmail")
+    if (regex.test(ctrl.value) === true) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inputAdressVerif(adressInput) {
+    let regex = /\w+(\s\w+){2,}/;
+    let ctrl = document.getElementById("inputAddress")
+    if (regex.test(ctrl.value) === true) {
+        return true
+    } else {
+        return false
+    }
+}
+function inputCityVerif(cityInput) {
+    let regex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+    let ctrl = document.getElementById("inputCity")
+    if (regex.test(ctrl.value) === true) {
+        return true
+    } else {
+        return false
+    }   
+}
+
+
+
 
 fetch("http://localhost:3000/api/cameras/")
     .then(res => res.json)
