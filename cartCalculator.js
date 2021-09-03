6862cfb3a91755a8f3a5ef8a394cf4c9a6082126
MasThomas <82974068+MasThomas@users.noreply.github.com>
2021-09-03 16:36:22 +0100

    var => let & commenting code

--- cartCalculator.js
+++ cartCalculator.js
@@ -1,12 +1,18 @@
-var cartToCalculate = JSON.parse(localStorage.getItem("cartStorage"))
+// On récupère le panier dans le localStorage
 
+let cartToCalculate = JSON.parse(localStorage.getItem("cartStorage"))
 
-var totalItemsInCart = 0;
+// On initialise la variable totalItemsInCart 
+let totalItemsInCart = 0;
 
 function calculateHowManyItemsInCart(){
+
+    // Si le cartStorage retourne "null" (vide), alors on retourne "0"
     if (localStorage.getItem("cartStorage") === null) {
         return totalItemsInCart = 0;
     }
+
+    // Sinon, on boucle sur le panier dans le localStorage pour récupérer la valeur "quantité" de chaque objet dans le panier
     else {
         for(let i = 0; i < cartToCalculate.length; ++i) {
         let j = Object.values(cartToCalculate[i])        
@@ -19,6 +25,8 @@ function calculateHowManyItemsInCart(){
 calculateHowManyItemsInCart();
 // console.log(totalItemsInCart)
 
-var itemsInCartSpan = document.getElementById("itemsInCart")
+// On vise le span "itemsInCart" présent dans le header pour afficher le résultat de totalItemsInCart
+let itemsInCartSpan = document.getElementById("itemsInCart")
 
+// Puis on actualise la valeur de celui-ci par le resultat de la fonction
 itemsInCartSpan.innerText = totalItemsInCart;
