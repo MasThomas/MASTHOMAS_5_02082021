20cc88a3b8d84df2203f141d3e67cd308c297fed
MasThomas <82974068+MasThomas@users.noreply.github.com>
2021-10-02 14:04:54 +0200

    spacing

--- confirmation.js
+++ confirmation.js
@@ -3,12 +3,18 @@ let containerErrorMessage = document.getElementById("container-orderNotOk")
 
 
 // On vérifie que "orderId" est présent dans le panier (et donc que l'étape précédente a été validée)
+
 function verifyIfOrderHasBeenPassed() {
     if (localStorage.getItem("orderId") === null) {
+
         // Si orderId n'existe pas, on supprime le container du message de confirmation de commande et l'utilisateur a un message d'erreur affiché
+        
         containerMessage.remove();
+    
     } else {
+
         // Si orderId existe, on récupère puis parse orderId et totalPrice
+        
     let orderId = JSON.parse(localStorage.getItem("orderId"))
     let totalPrice = JSON.parse(localStorage.getItem("totalPrice"))
         // On supprime le container du message d'erreur 
